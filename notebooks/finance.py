75ebcc1fc8fc829d2528e4015a830f2405658213
Aadya Trivedi <134907107+AadyaTrivedi@users.noreply.github.com>
2026-02-04 23:53:26 +0530

    Synthetic Finance Dataset

--- notebooks/finance.py
+++ notebooks/finance.py
@@ -1,8 +1,29 @@
-incomes = [30000, 45000, 60000]
+import random
+import pandas as pd
+
+incomes = []
 expenses = []
+savings = []
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-for income in incomes:
+
+for i in range(12):
+    income = random.randint(10000, 100000)
     expense = income * 0.7
-    expenses.append(expense)
+    save = income - expense
 
-print(expenses)
+    incomes.append(income)
+    expenses.append(expense)
+    savings.append(save)
+
+df = pd.DataFrame({
+    "month": months,
+    "monthly_income": incomes,
+    "monthly_expenses": expenses,
+    "monthly_savings": savings
+})
+
+
+df.to_csv("personal_finance_data.csv", index=False)
+
